971f329e9002268c5616472f1eed079a80b703c5
Dungeon-master06 <qet4upes@gmail.com>
2024-08-23 17:59:50 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# humanYears = 0
+# cat = 0
+# dog = 0
+# while True:
+#     if humanYears ==1:
+#         print(cat+15)
+#         print(dog+15)
+#         print(humanYears+1)
+#     elif humanYears ==2:
+#         print(cat+9)
+#         print(dog+9)
+#         print(humanYears+2)
+#     else:
+#         print(cat)
+#         print(dog)
+#         print(humanYears)
+
+
+
+
+
+
