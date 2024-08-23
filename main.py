f694ee8fb20de51462d391c657094ff6f24c3d71
Dungeon-master06 <qet4upes@gmail.com>
2024-08-23 18:10:56 +0300

    add new print

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-# humanYears = 0
-# cat = 0
-# dog = 0
-# while True:
-#     if humanYears ==1:
-#         print(cat+15)
-#         print(dog+15)
-#         print(humanYears+1)
-#     elif humanYears ==2:
-#         print(cat+9)
-#         print(dog+9)
-#         print(humanYears+2)
-#     else:
-#         print(cat)
-#         print(dog)
-#         print(humanYears)
-
+print('qwer')
+print ('Vladislev')
 
 
 
